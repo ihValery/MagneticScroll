e4755c95a484d9334eb4d008f50d54846f78df1d
ihValery <ihValery@gmail.com>
2022-06-04 16:01:47 +0300

    Initial Commit

---
+++ MagneticScroll/MagneticScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagneticScrollApp.swift
+//  MagneticScroll
+//
+//  Created by Валерий Игнатьев on 4.06.22.
+//
+
+import SwiftUI
+
+@main
+struct MagneticScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
